3c81c83f7e6b380732294731583410ead94cd7f1
Steve Taylor <steve@deployhub.com>
2023-08-31 15:39:56 -0600

    initial commit Signed-off-by: Steve Taylor <steve@deployhub.com>

---
+++ src/routes/ComponentVersionDetails.svelte
@@ -0,0 +1,28 @@
+
+<script>
+	import {onMount} from 'svelte';
+
+	const apiURL = "http://localhost:8080/msapi/compver/bafkreifw6fbrhbmcscd2bw2bz2gcmb4ge5fjh7rsvf3slygd3fugfym67y";
+	let compname = "";
+
+ 	onMount(async ()  => {
+		await fetch(apiURL).then(r => r.json()).then(data => {compname = data.domain.name + '.' + data.name;});
+  	});
+</script>
+
+<style>
+	article {
+		margin: 0 0 1em 0;
+	}
+
+	h1 {
+		font-size: 1.4em;
+		margin: 0;
+		display: block;
+	}
+</style>
+
+
+<article>
+		<h1>{compname}</h1>
+</article>
