a918e94f12c19b7432b2a331577a0f3e4b893408
Steve Taylor <steve@deployhub.com>
2023-10-05 18:00:54 -0600

    fix lint err Signed-off-by: Steve Taylor <steve@deployhub.com>

--- src/theme/create-dark-css.sh
+++ src/theme/create-dark-css.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-sed -e 's/@charset "UTF-8";/\.dark {/' -e 's/calc(0 -/calc(0% -/' < src/theme/work/smui-dark.css | awk '1; END {print "}"}' > src/theme/work/_smui-theme.scss
+sed -e 's/@charset "UTF-8";/\.dark {/' -e 's/calc(0 -/calc(0% -/' <src/theme/work/smui-dark.css | awk '1; END {print "}"}' >src/theme/work/_smui-theme.scss
 rm src/theme/work/smui-dark.css
