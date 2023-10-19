8d45eb227b3c3ce703e539c4e31faff6e5811c2e
Steve Taylor <steve@deployhub.com>
2023-10-19 10:58:17 -0600

    add parameter to InitializeDB Signed-off-by: Steve Taylor <steve@deployhub.com>

--- src/routes/compver/+page.svelte
+++ src/routes/compver/+page.svelte
@@ -2,14 +2,17 @@
   import { onMount } from "svelte";
   import Card, { Content } from "@smui/card";
   import { CompVerDetail } from "$lib/types";
-  import LayoutGrid, { Cell } from "@smui/layout-grid";
+  import LayoutGrid, { Cell as LayoutCell } from "@smui/layout-grid";
   import Textfield from "@smui/textfield";
   import { mdiOpenInNew } from "@mdi/js";
   import IconButton, { Icon } from "@smui/icon-button";
+  import Select, { Option } from '@smui/select';
+  import { Label } from '@smui/common';
   import { marked } from "marked";
   import { Svroller } from "svrollbar";
-  import DataTable, { Head, Body, Row, Cell as DtCell } from "@smui/data-table";
+  import DataTable, { Head, Body, Row, Cell, Pagination } from "@smui/data-table";
   import SwaggerUIBundle from "swagger-ui-dist/swagger-ui-bundle";
+  import { merge } from "lodash";
 
   function mapTextFields(detail: any) {
     let gitrepo = {
@@ -70,25 +73,37 @@
   let readme = "";
   let swagger = "";
   let license = "";
-  let vulns: any[] = [];
+  let pkgs: any[] = [];
+  let rowsPerPage = 6;
+  let currentPage = 0;
+
+  $: start = currentPage * rowsPerPage;
+  $: end = Math.min(start + rowsPerPage, pkgs.length);
+  $: lastPage = Math.max(Math.ceil(pkgs.length / rowsPerPage) - 1, 0);
+
+  $: if (currentPage > lastPage) {
+    currentPage = lastPage;
+  }
+
   let cells = mapTextFields(compverdetail);
   let compinfo = cells[0];
   cells = cells.slice(1);
   let swaggerUI;
 
-  const apiURL = "http://localhost:8080/msapi/compver/bafkreifw6fbrhbmcscd2bw2bz2gcmb4ge5fjh7rsvf3slygd3fugfym67y";
+  const apiURL = "http://localhost:8080/msapi/compver/bafkreif4ejff67pjs47qgvrnffymj24g7gkcb2hqf6geevttygqqpicbdu";
 
   onMount(async () => {
     await fetch(apiURL)
       .then((r) => r.json())
       .then((data) => {
-        compverdetail = data;
+        compverdetail = merge(compverdetail, data);
         cells = mapTextFields(compverdetail);
         compinfo = cells[0];
         cells = cells.slice(1);
         readme = (<string[]>compverdetail.readme.content).join("\n");
         license = (<string[]>compverdetail.license.content).join("\n");
         swagger = JSON.stringify(compverdetail.swagger.content);
+        pkgs = compverdetail.packages;
 
         swaggerUI = SwaggerUIBundle({
           dom_id: "#swagger-ui",
@@ -118,7 +133,7 @@
   <h2>{compverdetail.domain.name}.{compverdetail.name}</h2>
 </Content>
 <LayoutGrid>
-  <Cell>
+  <LayoutCell>
     <Card padded>
       <div class="cell_title">{compinfo.title}</div>
       {#each compinfo.fields as fld}
@@ -135,38 +150,58 @@
         {/if}
       {/each}
     </Card>
-  </Cell>
-  <Cell span={8}>
+  </LayoutCell>
+  <LayoutCell span={8}>
     <Card padded>
       <div class="cell_title">Licenses & Vulnerabilities</div>
-      <DataTable stickyHeader table$aria-label="Vulnerabilities" style="max-width: 100%;height: 28em;">
+      <DataTable style="width: 100%;height: 28em">
         <Head>
           <Row>
-            <DtCell>Package</DtCell>
-            <DtCell>Version</DtCell>
-            <DtCell>License</DtCell>
-            <DtCell>Id</DtCell>
-            <DtCell>Summary</DtCell>
+            <Cell>Package</Cell>
+            <Cell>Version</Cell>
+            <Cell>Language</Cell>
+            <Cell>License</Cell>
+            <Cell>CVE</Cell>
+            <Cell>Summary</Cell>
           </Row>
         </Head>
         <Body>
-          {#each vulns as fld}
+          {#each pkgs.slice(start, end) as fld}
             <Row>
-              <DtCell>{fld.package}</DtCell>
-              <DtCell>{fld.version}</DtCell>
-              <DtCell>{fld.license}</DtCell>
-              <DtCell>{fld.id}</DtCell>
-              <DtCell>{fld.summary}</DtCell>
+              <Cell>{fld.name}</Cell>
+              <Cell>{fld.version}</Cell>
+              <Cell>{fld.license}</Cell>
+              <Cell>{fld.language}</Cell>
+              <Cell>{fld.cve}</Cell>
+              <Cell>{fld.summary}</Cell>
             </Row>
           {/each}
         </Body>
+        <Pagination slot="paginate">
+          <svelte:fragment slot="rowsPerPage">
+            <Label>Rows Per Page</Label>
+            <Select variant="outlined" bind:value={rowsPerPage} noLabel>
+              <Option value={6}>6</Option>
+              <Option value={20}>20</Option>
+              <Option value={100}>100</Option>
+            </Select>
+          </svelte:fragment>
+          <svelte:fragment slot="total">
+            {start + 1}-{end} of {pkgs.length}
+          </svelte:fragment>
+
+          <IconButton class="material-icons" action="first-page" title="First page" on:click={() => (currentPage = 0)} disabled={currentPage === 0}>first_page</IconButton>
+          <IconButton class="material-icons" action="prev-page" title="Prev page" on:click={() => currentPage--} disabled={currentPage === 0}>chevron_left</IconButton>
+          <IconButton class="material-icons" action="next-page" title="Next page" on:click={() => currentPage++} disabled={currentPage === lastPage}>chevron_right</IconButton>
+          <IconButton class="material-icons" action="last-page" title="Last page" on:click={() => (currentPage = lastPage)} disabled={currentPage === lastPage}>last_page</IconButton>
+        </Pagination>
       </DataTable>
     </Card>
-  </Cell>
+  </LayoutCell>
 </LayoutGrid>
 <LayoutGrid>
   {#each cells as cell_detail}
-    <Cell>
+    <LayoutCell>
       <Card padded>
         <div class="cell_title">{cell_detail.title}</div>
         {#each cell_detail.fields as fld}
@@ -183,9 +218,9 @@
           {/if}
         {/each}
       </Card>
-    </Cell>
+    </LayoutCell>
   {/each}
-  <Cell>
+  <LayoutCell>
     <Card padded>
       <div class="cell_title">ReadMe</div>
       <Svroller width="100%" height="28em" alwaysVisible={true}>
@@ -194,16 +229,16 @@
         </div>
       </Svroller>
     </Card>
-  </Cell>
-  <Cell>
+  </LayoutCell>
+  <LayoutCell>
     <Card padded>
       <div class="cell_title">OpenApi</div>
       <Svroller width="100%" height="28em" alwaysVisible={true}>
         <div id="swagger-ui" />
       </Svroller>
     </Card>
-  </Cell>
-  <Cell>
+  </LayoutCell>
+  <LayoutCell>
     <Card padded>
       <div class="cell_title">License</div>
       <Svroller width="100%" height="28em" alwaysVisible={true}>
@@ -212,5 +247,5 @@
         </div>
       </Svroller>
     </Card>
-  </Cell>
+  </LayoutCell>
 </LayoutGrid>
