d430edbc3dc420515089ae493cf7c7b2f0db7f01
Steve Taylor <steve@deployhub.com>
2023-11-29 15:18:50 -0700

    add index.yaml Signed-off-by: Steve Taylor <steve@deployhub.com>

--- src/lib/swagger-ui-dist.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'swagger-ui-dist/swagger-ui-bundle' {
-    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-    const SwaggerUIBundle: any
-    export = SwaggerUIBundle;
-}
