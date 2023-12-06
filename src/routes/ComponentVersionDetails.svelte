8f21a86c6cc59b8f3ed9d2d773b6f0ba654b7ddc
Steve Taylor <steve@deployhub.com>
2023-12-06 13:24:16 -0700

    rework api url Signed-off-by: Steve Taylor <steve@deployhub.com>

--- src/routes/ComponentVersionDetails.svelte
+++ src/routes/ComponentVersionDetails.svelte
@@ -2,13 +2,11 @@
 <script>
 	import {onMount} from 'svelte';
 
-	let restapiURL = (process.env.RESTAPI_URL) ? process.env.RESTAPI_URL : "http://localhost:8080";
-
-	//const apiURL = "http://localhost:8080/msapi/compver/bafkreifw6fbrhbmcscd2bw2bz2gcmb4ge5fjh7rsvf3slygd3fugfym67y";
-	let apiURL = restapiURL + "/msapi/compver/bafkreie2tvmlendutm5gss7vrgr7jrd65mkuiqtfnugrdeujszuyaerrha";
 	let compname = "";
 
  	onMount(async ()  => {
+		let apiURL = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '') + "/msapi/compver/bafkreie2tvmlendutm5gss7vrgr7jrd65mkuiqtfnugrdeujszuyaerrha";
+
 		await fetch(apiURL).then(r => r.json()).then(data => {compname = data.domain.name + '.' + data.name;});
   	});
 </script>
