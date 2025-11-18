59acf1807bfa8f076b0de617d76982651d8d9f2c
Steve Taylor <steve@deployhub.com>
2025-11-18 15:08:40 -0700

    add openssf, git sections and icons Signed-off-by: Steve Taylor <steve@deployhub.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en">   
+      <head>
+        <link 
+          rel="stylesheet" 
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=bomb" 
+        />
+      </head>    
       <body>{children}</body>
     </html>
   )
--- lib/types.ts
+++ lib/types.ts
@@ -1,74 +1,58 @@
-export interface Vulnerability {
-  cve_id: string
-  summary: string
-  details: string
-  severity_score: number
-  severity_rating: string
-  cvss_v3_score?: string
-  published: string
-  modified: string
-  aliases: string[]
-  package: string
-  affected_version: string
-  full_purl: string
-  fixed_in: string[]
-}
-
-export interface AffectedRelease {
-  cve_id: string
-  summary: string
-  details: string
-  severity_score: number
-  severity_rating: string
-  published: string
-  modified: string
-  aliases: string[]
-  package: string
-  affected_version: string
-  full_purl: string
-  fixed_in: string[]
-  release_name: string
-  release_version: string
-  content_sha: string
-  project_type: string
-  dependency_count: number
-  synced_endpoint_count: number
-  openssf_scorecard_score: number
-}
-
-export interface AffectedEndpoint {
-  endpoint_name: string
-  endpoint_url: string
-  endpoint_type: string
-  environment: string
-  last_sync: string
-  status: string
-}
-
-export interface SyncedEndpoint {
-  endpoint_name: string
-  endpoint_url: string
-  endpoint_type: string
-  environment: string
-  status: string
-  last_sync: string
-  release_count: number
-  total_vulnerabilities: {
-    critical: number
-    high: number
-    medium: number
-    low: number
-  }
-  releases: Array<{
-    release_name: string
-    release_version: string
-  }>
-}
+// lib/types.ts
 
 export interface SBOM {
   key: string
   contentsha: string
+  objtype: string
   content: string
+}
+
+export interface Vulnerability {
+  cve_id: string
+  summary?: string
+  details?: string
+  severity_score?: number
+  severity_rating?: string
+  cvss_v3_score?: number
+  published?: string
+  modified?: string
+  aliases?: string[]
+  package: string
+  affected_version?: string
+  full_purl?: string
+  fixed_in?: string[]
+}
+
+export interface Documentation {
+  Short: string
+  URL: string
+}
+
+export interface Check {
+  Name: string
+  Score: number
+  Reason?: string
+  Details?: string[]
+  Documentation: Documentation
+}
+
+export interface Repo {
+  Name: string
+  Commit: string
+}
+
+export interface Scores {
+  Version: string
+  Commit: string
+}
+
+export interface ScorecardResult {
+  Date: string
+  Repo: Repo
+  Scorecard: Scores
+  Score: number
+  Checks: Check[]
+  Metadata?: string[]
 }
 
 export interface Release {
@@ -76,7 +60,7 @@ export interface Release {
   name: string
   version: string
   project_type: string
-  content_sha: string
+  content_sha?: string
   git_commit?: string
   git_branch?: string
   docker_repo?: string
@@ -84,61 +68,6 @@ export interface Release {
   docker_sha?: string
   sbom?: SBOM
   vulnerabilities: Vulnerability[]
-}
-
-export interface ImageData {
-  name: string
-  version: string
-  releaseDate: string
-  publisher: string
-  description: string
-  pulls: string
-  updated: string
-  verified: boolean
-  official: boolean
-  tags: string[]
-  longDescription?: string
-  vulnerabilities: {
-    critical: number
-    high: number
-    medium: number
-    low: number
-  }
-  dependency_count: number
-  signed: boolean
-  openssfScore: number
-  syncedEndpoints: number
-}
-
-export interface GetAffectedReleasesResponse {
-  affectedReleases: AffectedRelease[]
-}
-
-export interface GetReleaseResponse {
-  release: Release
-}
-
-export interface GetAffectedEndpointsResponse {
-  affectedEndpoints: AffectedEndpoint[]
-}
-
-export interface GetSyncedEndpointsResponse {
-  syncedEndpoints: SyncedEndpoint[]
-}
-
-export interface Mitigation {
-  cve_id: string
-  summary: string
-  severity_score: number
-  severity_rating: string
-  package: string
-  affected_version: string
-  full_purl: string
-  fixed_in: string[]
-  affected_releases: number
-  affected_endpoints: number
-}
-
-export interface GetMitigationsResponse {
-  mitigations: Mitigation[]
+  openssf_scorecard_score?: number
+  scorecard_result?: ScorecardResult
 }
