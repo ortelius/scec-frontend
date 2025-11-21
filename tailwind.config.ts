69c9eb61752eb25be78d5c87dc76f1f5c343abbf
Steve Taylor <steve@deployhub.com>
2025-11-21 09:03:21 -0700

    fix lint errs Signed-off-by: Steve Taylor <steve@deployhub.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,6 @@ import type { Config } from 'tailwindcss'
 export default {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+    './components/**/*.{js,ts,jsx,tsx,mdx}'
+  ]
 } satisfies Config
