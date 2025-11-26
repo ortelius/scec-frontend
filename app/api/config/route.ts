feb14d3c841214a8ebff0075dd8d8caab1f050f0
Steve Taylor <steve@deployhub.com>
2025-11-26 16:03:35 -0700

    fix lint errors Signed-off-by: Steve Taylor <steve@deployhub.com>

--- app/api/config/route.ts
+++ app/api/config/route.ts
@@ -2,8 +2,13 @@
 
 import { NextResponse } from 'next/server'
 
-export async function GET () {
+export async function GET (): Promise<NextResponse> {
+  const endpoint =
+    process.env.RUNTIME_GRAPHQL_ENDPOINT !== undefined
+      ? process.env.RUNTIME_GRAPHQL_ENDPOINT
+      : 'http://localhost:3000/api/v1/graphql'
+
   return NextResponse.json({
-    graphqlEndpoint: process.env.RUNTIME_GRAPHQL_ENDPOINT || 'http://localhost:3000/api/v1/graphql'
+    graphqlEndpoint: endpoint
   })
 }
